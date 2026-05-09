73b260565f9b1ff45d025847fa0dd836b4aba63e
craizytech <craizytech@gmail.com>
2026-05-09 22:59:49 +0300

    Updated the Item Doctype to ensure the valuation method field to be picked is the default one set in the Global Stock Settings Doctype

--- x_electronics_wms/x_electronics_warehouse_management_system/doctype/item/item.js
+++ x_electronics_wms/x_electronics_warehouse_management_system/doctype/item/item.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2026, Eammon Kiprotich and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Item", {
-// 	refresh(frm) {
+frappe.ui.form.on("Item", {
+	refresh(frm) {
+        if (frm.is_new()) {
+            frappe.db.get_single_value(
+                "Stock Settings",
+                "default_valuation_method"
+            ).then((value) => {
+                if (value) {
+                    frm.set_value("valuation_method", value);
+                }
+            });
+        }
 
-// 	},
-// });
+	},
+});

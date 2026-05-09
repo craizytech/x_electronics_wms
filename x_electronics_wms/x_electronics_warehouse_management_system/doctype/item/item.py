3af247354918e760823246da6cd86daf59f1aed9
craizytech <craizytech@gmail.com>
2026-05-09 23:02:20 +0300

    added validation to ensure that once an Item has been set to a specific valuation method, and there are stock entries against it. You cannot change the valuation method

--- x_electronics_wms/x_electronics_warehouse_management_system/doctype/item/item.py
+++ x_electronics_wms/x_electronics_warehouse_management_system/doctype/item/item.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2026, Eammon Kiprotich and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
@@ -21,4 +22,50 @@ class Item(Document):
 		valuation_method: DF.Literal["Moving Average", "FIFO", "LIFO"]
 	# end: auto-generated types
 
-	pass
+	def before_insert(self):
+		"""
+		Fetch Item valuation method from Stock Settings Doctype
+		"""
+		if not self.valuation_method:
+			self.valuation_method = self._get_default_valuation_method()
+	
+	def validate(self):
+		self.validate_valuation_method_not_changed()
+	
+	def _get_default_valuation_method(self):
+		"""
+		Fetch the default valuation otherwise set Moving Average to be the default valuation.
+		"""
+		try:
+			default = frappe.db.get_single_value(
+				"Stock Settings", "default_valuation_method"
+			)
+			return default or "Moving Average"
+		except Exception:
+			return "Moving Average"
+		
+	def validate_valuation_method_not_changed(self):
+		"""
+		This method ensures that once a stock ledger entry exists for an item
+		the valuation method cannot be changed.
+		"""
+		if self.is_new():
+			return
+		
+		old_method = frappe.db.get_value("Item", self.name, "valuation_method")
+
+		if old_method == self.valuation_method:
+			return
+		
+		sle_count = frappe.db.count(
+			"Stock Ledger Entry",
+			filters={"item": self.name, "docstatus": 1}
+		)
+
+		if sle_count > 0:
+			frappe.throw(_(
+				f"Cannot change valuation method for item '{self.name}'. "
+				f"This item has {sle_count} existing stock ledger entries. "
+				f"The valuation method can only be changed if no transactions exist. "
+				f"To change it, cancel all stock entries for this item first."
+			))
